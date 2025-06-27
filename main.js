9b098eb2bd45cf494ead53c80c4c5cba2e39e0f9
casianodossantoskracoski <160858438+casianodossantoskracoski@users.noreply.github.com>
2025-06-27 08:25:22 -0300

    .

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function geraSenha() {
     campoSenha.value = senha;
     classificaSenha();
 }
-function classificaSenha()
+function classificaSenha(){
 forcaSenha.classList.remove('fraca','media','forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
@@ -74,4 +74,5 @@ forcaSenha.classList.add('media');
 }
 else if (tamanhoSenha <= 5){
 forcaSenha.classList.add('fraca');
+}
 }
