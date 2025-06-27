e1c3d4f9db2efac31e49db02fa1a8476aec70976
casianodossantoskracoski <160858438+casianodossantoskracoski@users.noreply.github.com>
2025-06-27 07:56:03 -0300

    .

--- main.js
+++ main.js
@@ -68,5 +68,8 @@ forcaSenha.classList.remove('fraca','media','forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 }
+else {
+forcaSenha.classList.add('media');
+}
 forcaSenha.classList.add('forte');
 }
